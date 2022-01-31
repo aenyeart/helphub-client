58baf865c70f0ef0d037ea97ce6334bec6b41b96
Andrew Enyeart <reallyhardtoforget@gmail.com>
2022-01-30 18:51:29 -0800

    Adds remaining sub/pub functions and logic to worker and customer files

--- customer.js
+++ customer.js
@@ -24,13 +24,28 @@ customer.subscribe('connect', () => {
     console.log('Your help ticket # is', payload.id); // TODO dbl check var name against server
   });
 
+  customer.subscribe('Assigning Ticket', () => {
+    console.log('A service worker is now addressing your problem.');
+  });
+
   customer.subscribe('In-progress', () => {
     console.log('A service worker is now addressing your problem.');
   });
 
   customer.subscribe('Complete', () => {
     console.log('Your help request has been fulfilled.');
+    customer.disconnect();
   });
 
 });
+
+// when WORKER client signs in or completes a ticket, emits "standing by"
+// SERVER on 'standing by' pops next ticket off queue, assigns to that WORKER via payload
+// SERVER emits to CUSTOMER and WORKER 'assigning ticket'
+// on 'assigning ticket' WORKER emits in-progress
+// --> SERVER relays 'in-progress' to CUSTOMER
+// setTimeout, WORKER emits 'Complete'
+// --> SERVER relays 'complete' to CUSTOMER
+// --> CUSTOMER logs 'complete', disconnects socket
+// WORKER emits 'standing by'
 
--- worker.js
+++ worker.js
@@ -1,4 +1,47 @@
 'use strict';
 
 const Client = require('./lib/Client.js');
-const customer = new Client('/help', /*customer's name*/);
+const { workers } = require('./lib/mockData.js');
+const workerName = workers.pop();
+const worker = new Client('/help', workerName); // namespace is first arg to Client 
+
+worker.subscribe('connect', () => {
+  let payload = {
+    username: this.username,
+  };
+
+  let isAvailable = true; // Do I need this?
+
+  worker.subscribe('Ready for Request', (payload) => {
+    console.log('You are now connected to the Help Hub server via socket #', payload.customerSocket);
+
+    worker.publish('Standing By', payload);
+  });
+
+  worker.subscribe('Ticket Generated', () => {
+    if (isAvailable) {
+      worker.publish('Standing By', { worker: worker.username }); // TEST is worker.username a valid ref?
+    }
+  });
+
+  worker.subscribe('Assigning Ticket', (payload) => {
+    isAvailable = false;
+    worker.emit('In-Progress', payload);
+    setTimeout(() => {
+      worker.emit('Complete', payload);
+      isAvailable = true;
+      worker.emit('Standing By', { worker: worker.username });
+    }, 4000);
+  });
+
+});
+
+// when WORKER client signs in or completes a ticket, emits "standing by"
+// SERVER on 'standing by' pops next ticket off queue, assigns to that WORKER via payload
+// SERVER emits to CUSTOMER and WORKER 'assigning ticket'
+// on 'assigning ticket' WORKER emits in-progress
+// --> SERVER relays 'in-progress' to CUSTOMER
+// setTimeout, WORKER emits 'Complete'
+// --> SERVER relays 'complete' to CUSTOMER
+// --> CUSTOMER logs 'complete', disconnects socket
+// WORKER emits 'standing by'
