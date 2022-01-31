d35f1d26df8635bd07e493170daebeb9f80f4d4a
Andrew Enyeart <reallyhardtoforget@gmail.com>
2022-01-30 17:04:52 -0800

    Configures customer.js to pipe in mock data

--- customer.js
+++ customer.js
@@ -2,20 +2,24 @@
 // require in the { helpRequests } from mock
 // grab one from that array, use value of customer name below
 const Client = require('./lib/Client.js');
-const customer = new Client('/help', /*customer's name*/);
+const { helpRequests } = require('./lib/mockData.js');
+
+const newRequest = helpRequests.pop();
+
+const customer = new Client('/help', newRequest.username);
 
 customer.subscribe('connect', () => {
   let payload = {
-    username: 'BillyBobJr',
-    description: 'Help on lab 13',
+    username: this.username,
+    description: newRequest.description,
   };
   
-  customer.subscribe('In-progress', () => {
-    console.log('A service worker is now addressing your problem.');
+  customer.subscribe('Ready for Request', (payload) => {
+    console.log('You are now connected to the Help Hub server via socket #', payload.customerSocket);
+    
+    customer.publish('Help Requested', payload);
   });
 
-
-  customer.publish('Help Requested', payload);
   customer.subscribe('Ticket Generated', (payload) => {
     console.log('Your help ticket # is', payload.id); // TODO dbl check var name against server
   });
@@ -27,7 +31,6 @@ customer.subscribe('connect', () => {
   customer.subscribe('Complete', () => {
     console.log('Your help request has been fulfilled.');
   });
-
 
 });
 
